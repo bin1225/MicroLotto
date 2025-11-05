2114735e77a9a429c163fa0030d3eea252e7dafa
kkb <gimgyubin1225@gmail.com>
2025-11-05 18:47:57 +0900

    build(all): 프로젝트 별 포트 분리
    - 포트 충돌 회피를 위해 프로젝트 별 포트 분리

--- lotto-purchase-service/src/main/java/com/lotto/lotto_purchase_service/LottoPurchaseServiceApplication.java
+++ lotto-purchase-service/src/main/java/com/lotto/lotto_purchase_service/LottoPurchaseServiceApplication.java
@@ -2,8 +2,14 @@ package com.lotto.lotto_purchase_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.lotto.lotto_api",
+		"com.lotto.lotto_purchase_service",
+		"com.lotto.util"
+})
 public class LottoPurchaseServiceApplication {
 
 	public static void main(String[] args) {
