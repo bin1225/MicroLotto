66bb8f753b5aa50b6b42f9e5a0c38891c46ff10a
kkb <gimgyubin1225@gmail.com>
2025-11-05 17:13:28 +0900

    build(root): lotto-msa를 Gradle 루트 프로젝트로 설정하고 util 모듈 추가
    - settings.gradle을 루트 폴더에만 유지하도록 하위 파일 제거
    - 공용 유틸 기능을 위한 util 라이브러리 모듈 추가

---
+++ lotto-api/src/test/java/com/lotto/lotto_api/LottoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotto.lotto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
